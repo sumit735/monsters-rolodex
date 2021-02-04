b58180afe16504282cd3a33372cdf97b77ceb681
sumit735 <sumitkumardash1999@gmail.com>
2021-02-04 18:22:44 +0530

    Updates

---
+++ precache-manifest.611368f492ef7d9918a72d582bf512f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1184dfb8b2b2979488f570d83bba8afe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1e73ba6c3b97c1b27665",
+    "url": "/monsters-rolodex/static/css/main.199e85f2.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e73ba6c3b97c1b27665",
+    "url": "/monsters-rolodex/static/js/main.fc751909.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
